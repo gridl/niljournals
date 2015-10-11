3c1f3adb94834d935ab0a9d71894eca6e4fef4d4
Ole Weidner <ole.weidner@codewerft.net>
2015-10-11 16:58:40 +0200

    working base structure

--- README.rst
+++ README.rst
@@ -2,3 +2,8 @@ niljournals
 ===========
 
 ProQuest to CSV conversion and filter tool.
+
+Installation
+------------
+
+Open a Terminal window (on OS X: Finder -> Applications -> Utilities -> Terminal)
--- setup.py
+++ setup.py
@@ -18,22 +18,22 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='sample',
+    name='niljournals',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.2.0',
+    version='1.0.0',
 
-    description='A sample Python project',
+    description='ProQuest to CSV conversion and filter tool',
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/pypa/sampleproject',
+    url='https://github.com/oweidner/niljournals',
 
     # Author details
-    author='The Python Packaging Authority',
-    author_email='pypa-dev@googlegroups.com',
+    author='Nil Uzun, Ole Weidner',
+    author_email='nuzun@sociology.rutgers.edu',
 
     # Choose your license
     license='MIT',
@@ -58,10 +58,10 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.2',
+        # 'Programming Language :: Python :: 3.3',
+        # 'Programming Language :: Python :: 3.4',
     ],
 
     # What does your project relate to?
@@ -75,7 +75,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['peppercorn'],
+    install_requires=['xlrd'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
@@ -97,14 +97,14 @@ setup(
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('my_data', ['data/data_file'])],
+    data_files=[('my_data', ['data/ProQuestDocuments-2015-10-02-2.xls'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'sample=sample:main',
+            'journalfilter=journalfilter:main',
         ],
     },
 )
